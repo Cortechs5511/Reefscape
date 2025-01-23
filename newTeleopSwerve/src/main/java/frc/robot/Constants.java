6d569b3de19e4ccfa9dd1cf8568316c6a978b008
Isaac Yuan <ieyuan1608@gmail.com>
2025-01-22 20:02:38 -0500

    oops forgot about constants
    Co-Authored-By: Ryan-919 <68666490+Ryan-919@users.noreply.github.com>

--- newTeleopSwerve/src/main/java/frc/robot/Constants.java
+++ newTeleopSwerve/src/main/java/frc/robot/Constants.java
@@ -27,6 +27,21 @@ public final class Constants {
     public static final double DEADBAND = 0.1;
   }
 
+  public static class ElevatorConstants {
+    public static final int WRIST_L_ID = 60;
+    public static final int WRIST_R_ID = 61;
+
+    public static final double[] PID_VALUES = {0.25, 0.3, 0.0};
+
+    public static final double POSITION_CONVERSION_FACTOR = 50 * 22 / 12;
+
+    public static final double VOLTAGE_COMPENSATION = 5;
+    public static final int CURRENT_LIMIT = 40;
+    public static final IdleMode IDLE_MODE = IdleMode.kBrake;
+    public static final double RAMP_RATE = 0.05;
+
+  }
+
   public static class SwerveConstants {
 
     // Motor Controller and Encoder Configuration
